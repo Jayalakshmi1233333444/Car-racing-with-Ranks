d1637d7a19dbaa7cadecb8b720dde9ae644753ff
narayanan <sj10773@rediffmail.com>
2020-12-21 18:20:29 +0530

    we have done some changes

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1image);
     car2 = createSprite(300,200);
+    car2.addImage(car2image);
     car3 = createSprite(500,200);
+    car3.addImage(car3image);
     car4 = createSprite(700,200);
+    car4.addImage(car4image);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background(ground);
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -63,7 +68,10 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+         stroke("black");
+         strokeWeight(4);
+         fill("Blue");
+          ellipse(x,y,60,60);
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
         }
@@ -76,9 +84,15 @@ class Game {
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=10
+      console.log(player.distance);
       player.update();
     }
-
+if(player.distance>3860){
+  gameState=2
+}
     drawSprites();
+  }
+  end(){
+    console.log("GAME ENDED")
   }
 }
--- sketch.js
+++ sketch.js
@@ -5,12 +5,21 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
-
+var track,car1image,car2image,car3image,car4image,ground;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+function preload(){
+  car1image=loadImage("images/car1.png");
+  car2image=loadImage("images/car2.png");
+  car3image=loadImage("images/car3.png");
+  car4image=loadImage("images/car4.png");
+  ground=loadImage("images/ground.png");
+  track=loadImage("images/track.jpg");
+  
 
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +36,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
